54f72cac323bdb0461f5b7e28b31a07a85ddc008
Gustavo Machado <gustavo.machado@playkids.com>
2021-10-15 15:30:53 -0300

    Added screen flow feature

---
+++ ScreenFlow/PopupConfig.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace LegendaryTools.Systems.ScreenFlow
+{
+    [CreateAssetMenu(menuName = "CustomTools/ScreenFlow/PopupConfig")]
+    public class PopupConfig : UIEntityBaseConfig
+    {
+        public bool HideWhenGoingToBackground;
+    }
+}
